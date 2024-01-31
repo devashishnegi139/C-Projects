e111a08c1b0b822174cd035b9124e9695f0fc09d
Devashish Negi <devashishnegi113399@gmail.com>
2024-01-31 18:04:10 +0530

    Completed the code

--- paddingTest/main.c
+++ paddingTest/main.c
@@ -2,14 +2,23 @@
 #include <stdint.h>
 
 struct test{
+    // put your varables here
     uint32_t var1;
     uint8_t var2;
-    uint16_t var3;
-    char var4;
+    uint8_t var3;
+    uint8_t var4;
 };
 
 int main(){
+    // fill values in t1 accoudingly
     struct test t1 = {0xaa, 0xbb, 0xcc, 0xdd};
-    
+    char *p = (char *)&t1;
+
+    for(unsigned long long i=0; i<sizeof(struct test); i++){
+        printf("%p\t%x\n", p, *p);
+        if(i!=0 && (i+1)%4==0)
+            printf("-----------------------------------------------\n");
+        p++;
+    }
     return 0;
 }
